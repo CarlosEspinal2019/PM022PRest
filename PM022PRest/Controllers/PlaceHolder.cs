460e0b9b9936208ac7d752aaff3bb55dc8046011
Carlos Espinal <carloseduardoespinal@outlook.com>
2023-02-22 22:36:10 -0600

    Agregar archivos de proyecto.

---
+++ PM022PRest/Controllers/PlaceHolder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PM022PRest.Models;
+
+namespace PM022PRest.Controllers
+{
+    public static class PlaceHolder
+    {
+        public async static Task<List<Models.Posts>> GetPosts();
+        {
+            List<Models.Posts> posts = new List<Models.Posts>();
+            using(HttpClient client = new HttpClient())
+        {
+           var response = await ClientCertificateOption.GetAsync("https://jsonplaceholder.typicode.com/posts"); 
+            
+            if (reponse.IsSuccessStatusCode)     
+        {
+            posts = JsonConvert.DeserializedObjet<List<Models.Posts>>(reponse.)
+        }
+        
+        }
+        return posts;
+        }
+    }
+}
